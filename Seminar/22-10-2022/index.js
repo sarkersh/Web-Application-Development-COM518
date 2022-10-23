52cffd59b4bc3aa47389376b33d6e30469f3330b
sarkersh <1sarkm21@solent.ac.uk>
2022-10-24 00:34:34 +0100

    topic1
    topic1

--- Seminar/22-10-2022/index.js
+++ Seminar/22-10-2022/index.js
@@ -1,23 +1,58 @@
-var x = parseInt(prompt("Enter the starting number: "));
-var y = parseInt(prompt("Enter the ending number: "));
 
+for (var i = 1; i<=100; i=i+1){
+    if (i  % 3 == 0) {
+        continue;
+    }
+    document.write(" " + i);
+}
+document.write("<h1>End</h1>");
+
+
+// for (var i = 1; i<=100; i=i+1){
+//     if (i == 19){
+//         break;
+//     }
+//     document.write(" " + i);
+// }
+// document.write("<h1>End</h1>");
+
+
+
+// var i = 11;
+
+// do{
+//     document.write("  " + i );
+//     i++;
+   
+// } while(i <= 50);
+
+
+
+
+
+// // var x = parseInt(prompt("Enter the starting number: "));
+// // var y = parseInt(prompt("Enter the ending number: "));
+
+// // var s = 0;
+
+// // for(var i=x; i<=y; i=i+1)
+// // {
+// //     s = s + i;
+// // }
+
+
+
+
+// // document.write("Result :" + s);
+
+// var i = 1;
 // var s = 0;
 
-// for(var i=x; i<=y; i=i+1)
-// {
-//     s = s + i;
+// while(i <= 100){
+//     if (i % 3 == 0 && i % 5 == 0){
+//         document.write("  " + i + "<br/>")
+//         s = s + i
+//     }
+//     i++;
 // }
-
-
-
-
 // document.write("Result :" + s);
-
-var i = x;
-var s = 0;
-
-while(i <= y){
-    s = s + i
-    i++;
-}
-document.write("Result :" + s);
