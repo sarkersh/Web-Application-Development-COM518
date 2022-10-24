383c98012d64b69aed4b172321f8e05687346666
sarkersh <1sarkm21@solent.ac.uk>
2022-10-24 02:50:57 +0100

    zz

--- webApp/app.js
+++ webApp/app.js
@@ -3,7 +3,15 @@ const app = express();
 port = 3000;
 
 app.get('/', (req, res) => {
-    res.send('Hello World from Express');
+    res.send('<h1>Hello World from Express</h1>');
+});
+
+app.get('/hello', (req, res) => {
+    res.send('<h1>Hello Worl!!!</h1>');
+});
+
+app.get('/time', (req, res) => {
+    res.send('<h1>Hello World from Express</h1>');
 });
 
 app.listen(port);
