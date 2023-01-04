9c8e77244f2a9750aec66ada840c5e5350be750d
sarkersh <1sarkm21@solent.ac.uk>
2023-01-04 01:42:03 +0000

    Mysql Conected
    Mysql Conected

---
+++ Points of Interest Application/config/dbConnection.js
@@ -0,0 +1,15 @@
+require("dotenv").config();
+import mysql from "mysql2";
+
+let connection = mysql.createConnection({
+    host: process.env.DB_HOST,
+    database: process.env.DB_NAME,
+    user: process.env.DB_USERNAME,
+    password: process.env.DB_PASSWORD
+});
+
+connection.connect(() => {
+    console.log("Mysql DataBase Successfully Connected.");
+});
+
+module.exports = connection;
--- Points of Interest Application/server.js
+++ Points of Interest Application/server.js
@@ -1,5 +1,6 @@
 import express from "express";
 import viewEngine from "./config/viewEngine";
+import connection from "./config/dbConnection";
 import initWebRoutes from "./routes/route";
 
 let app = express();
