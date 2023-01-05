a58620248c314434f19d2f58243e6bc0e1712c60
sarkersh <1sarkm21@solent.ac.uk>
2023-01-05 06:10:45 +0000

    bcrypt not working

--- Points of Interest Application/services/registerService.js
+++ Points of Interest Application/services/registerService.js
@@ -1,8 +1,18 @@
+import bcrypt from "bcryptjs";
 import connection from "../config/dbConnection";
 
 let createNewUser = (user) => {
     return new Promise ((resolve, reject) => {
         try {
+            //hasing a password using bcrypt
+            let salt = bcrypt.genSaltSync(10);
+            let data = {
+                fullname: user.fullname,
+                email: user.email,
+                username: user.username,
+                password: bcrypt.hashSync(user.password, salt)
+            };
+            //create a new user
             connection.query("INSERT INTO poi_users set ? ", user, function(error, rows){
                 if(error) reject(error);
                 resolve("Successfully User Createed");      
