c1393a161dfb290ac39e46775ebb864443536e5f
sarkersh <1sarkm21@solent.ac.uk>
2023-01-07 02:22:59 +0000

    Update index.js

--- pointsOfInterest/index.js
+++ pointsOfInterest/index.js
@@ -2,6 +2,8 @@ const express = require("express");
 
 const app = express();
 const port = 9000;
+app.use(express.json());
+app.use(express.urlencoded({extended: true}));
 
 
 app.get("/", (req, res) => {
