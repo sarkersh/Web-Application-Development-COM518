0d3a488149b137aa0daf603ef0a0f887a33acdf0
sarkersh <1sarkm21@solent.ac.uk>
2023-01-07 21:38:20 +0000

    vv

--- Points of Interest Application/public/js/register.js
+++ Points of Interest Application/public/js/register.js
@@ -4,13 +4,13 @@
 default return false, return true if has errors
 */
 
-function validateInput(userName, password, passwordConfirmation) {
+function validateInput(email, password, passwordConfirmation) {
     //check user name
-    const USER_REG = /[a-zA-Z][a-zA-Z0-9_\.]{1,32}@[a-z0-9]{2,}(\.[a-z0-9]{2,4}){1,2}/g;
-    if(userName.match(USER_REG)) {
-        $("#userName").removeClass("is-invalid");
-    } else {//empty username input or invalid username
-        $("#userName").addClass("is-invalid");
+    const EMAIL_REG = /[a-zA-Z][a-zA-Z0-9_\.]{1,32}@[a-z0-9]{2,}(\.[a-z0-9]{2,4}){1,2}/g;
+    if(email.match(EMAIL_REG)) {
+        $("#email").removeClass("is-invalid");
+    } else {//empty email input or invalid email
+        $("#email").addClass("is-invalid");
     }
 
     //check password
@@ -27,7 +27,7 @@ function validateInput(userName, password, passwordConfirmation) {
         $("#passwordConfirmation").addClass("is-invalid");
     }
 
-    if(!userName.match(USER_REG) || password.length <= 2 || password !== passwordConfirmation)
+    if(!email.match(EMAIL_REG) || password.length <= 2 || password !== passwordConfirmation)
         return true; //has errors
 
     return false;
